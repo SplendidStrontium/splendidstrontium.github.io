e890a4b6e4e71389f29bdae908debe87bf25950b
Paige <pnkrsnak@gmail.com>
2016-01-11 08:53:31 -0500

    removed roll pg

--- roll/roll.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('dieresult').setAttribute('value', '3') ;
-
-function roll() {
-	alert("rolled!");
-}
-
-$( document ).ready( function() {
-  document.getElementById('dieresult').setAttribute('value', '3') ;
-});
