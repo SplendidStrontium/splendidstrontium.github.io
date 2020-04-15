c7297b9c5f533f2a7ae1c8d0872cecca9f37cf3e
Paige <pnkrsnak@gmail.com>
2020-04-15 16:52:13 -0400

    titlefix, canvas talks to js

---
+++ assets/cc.js
@@ -0,0 +1,15 @@
+// CONCENTRIC CIRCLES
+
+window.onload = function() {
+
+	var c = document.getElementById("ccCanvas");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,200);
+	ctx.stroke();
+
+}
+
+
+
+
