514c42dbde148e53856c312fd8618006b8e14921
Paige Krsnak <nan@nan.nyc>
2021-09-09 13:41:01 -0400

    d20srdmonsters skeleton

--- assets/mapgen.js
+++ assets/mapgen.js
@@ -14,20 +14,23 @@ window.onload = function() {
 	var ctx = canvas.getContext("2d");
 
 	var drawGrid = function(ctx, w, h, step){
+		// horizontal grid lines
 		ctx.beginPath();
 		for (var x=0;x<=w;x+=step) {
-			ctx.moveTo(x, 0);
-			ctx.lineTo(x, h);
+			ctx.moveTo(x+0.5, 0);
+			ctx.lineTo(x+0.5, h);
 		}
 		// set the color of the line
 		ctx.strokeStyle = 'rgb(150,150,150)';
 		ctx.lineWidth = 1;
 		// the stroke will actually paint the current path
 		ctx.stroke();
+		
+		// vertical grid lines
 		ctx.beginPath();
 		for (var y=0;y<=h;y+=step) {
-			ctx.moveTo(0, y);
-			ctx.lineTo(w, y);
+			ctx.moveTo(0, y+0.5);
+			ctx.lineTo(w, y+0.5);
 		}
 		// set the color of the line
 		ctx.strokeStyle = 'rgb(150,150,150)';
@@ -40,6 +43,16 @@ window.onload = function() {
 
 	drawGrid(ctx, w, h, step);
 
+}
+
+function submitMapGen() {
+	//var monsters = {{ site.data.monsters.monsters | jsonify }};
+	var resultsdetail = document.getElementById("mapgen-results");
+	var mnstramt = document.getElementById("monster-amount").value;
+	var resultsdetailline1 = "You generated " + mnstramt + " monster(s)";
+	
+	document.getElementById("mapgen-results").innerHTML = resultsdetailline1;
+	//document.getElementById("mapgen-results").innerHTML = monsters;
 }
 
 
