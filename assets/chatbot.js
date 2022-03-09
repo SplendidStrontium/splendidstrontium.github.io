72b730d9c229d5c6e91d3e47223acc21204bbf13
Paige <nan@nan.nyc>
2022-03-09 11:22:51 -0500

    chatbot skeleton

---
+++ assets/chatbot.js
@@ -0,0 +1,11 @@
+---
+---
+
+//FPMC CHATBOT
+
+//BEGIN bootup greeting
+function chatbotbootup() {
+	document.getElementById("chatbot-txta").value = "Hello! Welcome to our Chatbot!\n\nDevelopment in progress as of March 9, 2022.";
+}
+//END bootup greeting
+
